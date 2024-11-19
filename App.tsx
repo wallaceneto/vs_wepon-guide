4c9aedc9a394d64a3b7686c150c5848523ea88ad
Wallace Neto <wallace.neto@outlook.com>
2024-11-19 13:01:12 -0300

    feat: adding castlevania sprites

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Image } from 'expo-image';
 
 import item from './assets/passive-items/Sprite-Skull_OManiac.webp';
-import wepom from './assets/wepons/Sprite-Runetracer.webp';
+import wepom from './assets/castlevania/Sprite-Iron_Shield.webp';
 
 export default function App() {
   return (
