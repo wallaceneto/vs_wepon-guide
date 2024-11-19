550a11f7038ab629e6280a2f25031d74cc6c2d11
Wallace Neto <wallace.neto@outlook.com>
2024-11-19 13:16:59 -0300

    feat: adding evolutions sprites

--- App.tsx
+++ App.tsx
@@ -2,8 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Image } from 'expo-image';
 
-import item from './assets/passive-items/Sprite-Skull_OManiac.webp';
-import wepom from './assets/castlevania/Sprite-Iron_Shield.webp';
+import wepom from './assets/evolutions/base/Sprite-Valkyrie_Turner.webp';
 
 export default function App() {
   return (
