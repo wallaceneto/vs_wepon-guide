a49fb48f4418dd51a3fccbdbe563255d08b8c58b
Wallace Neto <wallace.neto@outlook.com>
2024-11-28 10:13:23 -0300

    chore: adding more passive sprites

--- global/types/IPassiveSpriteKeys.ts
+++ global/types/IPassiveSpriteKeys.ts
@@ -1,2 +1,5 @@
 export type IPassiveSpriteKeys = 
-'Hollow_Heart' | 'Empty_Tome' | 'Bracer' | 'Candelabrador';
+'Hollow_Heart' | 'Empty_Tome' | 'Bracer' | 'Candelabrador' | 'Clover' |
+'Spellbinder' | 'Spinach' | 'Pummarola' | 'Attractorb' | 'Armor' |
+'Duplicator' | 'Crown' | 'Stone_Mask' | 'Skull_OManiac' | 'Wings' |
+'Tirajisu';
